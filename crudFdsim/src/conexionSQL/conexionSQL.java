030c59fae307c47e14973428d2a05df0fc15b7da
Juanbonillatavera <101885071+Juanbonillatavera@users.noreply.github.com>
2022-08-25 11:41:35 -0500

    Crud del instructivo

---
+++ crudFdsim/src/conexionSQL/conexionSQL.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexionSQL;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class conexionSQL {
+
+    Connection conectar = null;
+
+    public Connection conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/adsi", "root","");
+            JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error de conexion" + e.getMessage());
+
+        }
+        return conectar;
+    }
+}
